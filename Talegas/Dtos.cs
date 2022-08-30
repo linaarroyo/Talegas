73018e9dfaaefe551265d4bd98213d82c26de794
Lina Arroyo <lmarroyoj@gmail.com>
2022-08-30 14:01:01 -0500

    Agregué todos los archivos de Talegas.

---
+++ Talegas/Dtos.cs
@@ -0,0 +1,14 @@
+﻿namespace Talegas
+{
+    public class LoginDto
+    {
+        public string email;
+        public string password;
+    }
+    public class LoginUser
+    {
+        public string email;
+        public string password;
+    }
+
+}
